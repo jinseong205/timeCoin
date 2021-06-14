61f3a91d22789490c9cabbafbdcbe9efe44e5214
jinseong <jinseong@DESKTOP-RFE7VCD>
2021-06-14 15:29:49 +0900

    test

--- src/main/java/com/jinseong/test/programmers/hash/lv1_failureComplete.java
+++ src/main/java/com/jinseong/test/programmers/hash/lv1_failureComplete.java
@@ -1,8 +1,8 @@
 package com.jinseong.test.programmers.hash;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
-//fail 50/100
+//sucess 100/100
 public class lv1_failureComplete {
 
 	public static void main(String[] args) {
@@ -15,6 +15,26 @@ public class lv1_failureComplete {
 	
 	public String solution(String[] participant, String[] completion) {
         
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		
+		String answer = "";
+		
+		for(int i = 0; i < participant.length -1 ; i++) {
+			//System.out.println("idx" + i + "  " + participant[i] + " " + completion[i]);
+			
+			if(!participant[i].equals(completion[i])) {
+				
+				answer = participant[i];
+				break;
+			}
+		}
+		if(answer.equals("")) {
+			answer = participant[participant.length - 1];
+		}
+		
+		
+		/*	50.0/100.0
 		ArrayList<String> list = new ArrayList<String>();
 		
 		for(String s : participant) {
@@ -23,9 +43,9 @@ public class lv1_failureComplete {
 		for(String s: completion) {
 			list.remove(s);
 		}
-		
-		
 		String answer = list.get(0);
-        return answer;
+		*/
+		
+		return answer;
     }
 }
